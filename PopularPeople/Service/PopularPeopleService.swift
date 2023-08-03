865d0dd1ce199306498105fce803519bc44c4d6c
NadaAshraf <nadaa8709@gmail.com>
2023-08-03 21:51:54 +0300

    get popular persons service

--- PopularPeople/Service/PopularPeopleService.swift
+++ PopularPeople/Service/PopularPeopleService.swift
@@ -6,3 +6,55 @@
 //
 
 import Foundation
+
+protocol PopularPeopleService : class {
+    func fetchPopularPeople(pageNumber: Int, completion: @escaping ([Any]?) -> Void)
+}
+
+class PopularPeopleServiceImpl : PopularPeopleService {
+    
+    let popularPeopleURL = "https://api.themoviedb.org/3/person/popular?api_key=e7631ffcb8e766993e5ec0c1f4245f93&language=en-US&page=%@"
+    
+    let defaultSession = URLSession(configuration: .default)
+    
+    var dataTask: URLSessionDataTask?
+    var errorMessage = ""
+        
+    func fetchPopularPeople(pageNumber: Int, completion: @escaping ([Any]?) -> Void) {
+        dataTask?.cancel()
+        
+        guard let url = URL(string: String(format: popularPeopleURL, "\(pageNumber)") ) else {
+            return
+        }
+        
+        dataTask = defaultSession.dataTask(with: url) { [weak self] data, response, error in
+            defer {
+                self?.dataTask = nil
+            }
+            
+            if let error = error {
+                self?.errorMessage += "DataTask error: " + error.localizedDescription + "\n"
+            } else if let data = data {
+                
+                var response: [String: Any]?
+                
+                do {
+                  response = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                } catch _ as NSError {
+                  return
+                }
+                
+                guard let array = response!["results"] as? [Any] else {
+                  return
+                }
+                
+                DispatchQueue.main.async {
+                    completion(array)
+                }
+            }
+        }
+        
+        dataTask?.resume()
+    }
+}
+
